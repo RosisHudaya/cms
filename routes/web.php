12c3fe3df8e6e7b39d8bb674a413ee1149ef2e7f
hudaya2002 <budistory25@gmail.com>
2022-02-23 14:45:55 +0700

    Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,4 +34,8 @@ use App\Http\Controllers\PageController;
 // PRAKTIKUM 2
 // Route::get('/', [PageController::class, 'index']);
 // Route::get('/', [PageController::class, 'about']);
-Route::get('/', [PageController::class, 'articles']);
+// Route::get('/', [PageController::class, 'articles']);
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
